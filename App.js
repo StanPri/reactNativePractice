0fc2d33d281bef209cf8c1537aaffe851f2aa6dd
Stan <pribytkovskystan@careinnovations.com>
2023-08-24 14:40:53 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => { 
+        setUsers(response)
+        console.log(JSON.stringify(response, undefined, 4))
+      })
+
+
+  }, [])
+
+  const itemComponent = (item) => {
+    return <Text style={styles.item}>{item.name}</Text>
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <FlatList 
+        data={users}
+        renderItem={({item}) => itemComponent(item)}
+        />
+        
     </View>
   );
 }
@@ -13,8 +37,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
+
